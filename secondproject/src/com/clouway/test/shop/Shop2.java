9b96b8ce17a43b57def17da0eb4aca6458be5e09
ddimitrov123 <dimitar.dimitrov045@gmail.com>
2014-10-24 17:47:29 +0300

    24.10.2014

--- secondproject/src/com/clouway/test/shop/Shop2.java
+++ secondproject/src/com/clouway/test/shop/Shop2.java
@@ -15,40 +15,51 @@ public class Shop2 {
   Map<String, Product> products = new HashMap<String, Product>();
   private List<Double> priceList = new ArrayList<Double>();
 
+  public List<Double> getPriceList() {
+    return priceList;
+  }
+
   public void addProduct(String name, Product product) {
+    if (product.getMaxQuantity() < product.getQuantity()) {
+      throw new AddMoreThanMaxQuantityException();
+    }
     products.put(name, product);
+    currentQuantity = products.get(name).getQuantity();
     priceList.add(products.get(name).getPrice());
   }
 
   public int sell(String name, int quantity) {
-    if(!products.containsKey(name)){
+    if (!products.containsKey(name)) {
       throw new ProductNotFoundException();
+    }
+    if(quantity > currentQuantity){
+      throw new SellMoreThanMinQuantityException();
     }
     if (counter > 0) {
       currentQuantity = currentQuantity - quantity;
-    }else {
+    } else {
       currentQuantity = products.get(name).getQuantity() - quantity;
     }
     return currentQuantity;
   }
 
   public int add(String name, int quantity) {
+
     if (products.containsKey(name)) {
       currentQuantity = products.get(name).getQuantity() + quantity;
-    }else{
+    } else {
       throw new ProductNotFoundException();
     }
-
+    int temp = products.get(name).getQuantity() + quantity;
+    if(temp > products.get(name).getMaxQuantity()){
+      throw new AddMoreThanMaxQuantityException();
+    }
     counter++;
     return currentQuantity;
   }
- 
+
   public List sort() {
     Collections.sort(priceList);
-   return priceList;
-  }
-
-  public List<Double> getPriceList() {
     return priceList;
   }
 }
--- secondproject/test/ShopTest2.java
+++ secondproject/test/ShopTest2.java
@@ -1,5 +1,7 @@
+import com.clouway.test.shop.AddMoreThanMaxQuantityException;
 import com.clouway.test.shop.Product;
 import com.clouway.test.shop.ProductNotFoundException;
+import com.clouway.test.shop.SellMoreThanMinQuantityException;
 import com.clouway.test.shop.Shop2;
 import org.junit.Before;
 import org.junit.Test;
@@ -45,6 +47,20 @@ public class ShopTest2 {
     shop.sell("Orange",10);
   }
 
+  @Test(expected = SellMoreThanMinQuantityException.class)
+  public void sellMoreThanWeHave() throws Exception {
+    shop.addProduct("Apple", new Product(2.3, 13, 100));
+    shop.sell("Apple", 14);
+  }
+
+  @Test(expected = SellMoreThanMinQuantityException.class)
+  public void sellMoreThanWeHaveWithAdd() throws Exception {
+    shop.addProduct("Apple", new Product(2.3, 10, 100));
+    shop.add("Apple", 20);
+    shop.sell("Apple", 40);
+
+  }
+
   @Test
   public void addQuantityToProduct() throws Exception {
     shop.addProduct("Kiwi", new Product(1.6, 10, 100));
@@ -64,6 +80,17 @@ public class ShopTest2 {
     shop.add("Orange", 10);
   }
 
+  @Test(expected = AddMoreThanMaxQuantityException.class)
+  public void addProductMoreQuantityThanMaxQuantity() throws Exception {
+    shop.addProduct("Kiwi", new Product(1.6, 110, 100));
+  }
+
+  @Test(expected = AddMoreThanMaxQuantityException.class)
+  public void addMoreQuantityThanMaxQuantity() throws Exception {
+    shop.addProduct("Kiwi", new Product(1.6, 50, 100));
+    shop.add("Kiwi", 60);
+  }
+
   @Test
   public void sortByPrice() throws Exception {
     shop.addProduct("Kiwi", new Product(1.6, 10, 100));
@@ -72,11 +99,25 @@ public class ShopTest2 {
     shop.addProduct("Banana", new Product(2.3, 25, 100));
     shop.addProduct("Cherry", new Product(3.5, 18, 100));
     shop.sort();
-    assertThat(shop.getPriceList().get(0), is(1.2) );
-    assertThat(shop.getPriceList().get(1), is(1.6) );
-    assertThat(shop.getPriceList().get(2), is(2.0) );
-    assertThat(shop.getPriceList().get(3), is(2.3) );
+    assertThat(shop.getPriceList().get(0), is(1.2));
+    assertThat(shop.getPriceList().get(1), is(1.6));
+    assertThat(shop.getPriceList().get(2), is(2.0));
+    assertThat(shop.getPriceList().get(3), is(2.3));
     assertThat(shop.getPriceList().get(4), is(3.5) );
+  }
 
+  @Test
+  public void anotherSortByPrice() throws Exception {
+    shop.addProduct("Kiwi", new Product(2.2, 10, 100));
+    shop.addProduct("Orange", new Product(2.3, 15, 100));
+    shop.addProduct("Apple", new Product(1.8, 12, 100));
+    shop.addProduct("Banana", new Product(0.5, 25, 100));
+    shop.addProduct("Cherry", new Product(6.2, 18, 100));
+    shop.sort();
+    assertThat(shop.getPriceList().get(0), is(0.5));
+    assertThat(shop.getPriceList().get(1), is(1.8));
+    assertThat(shop.getPriceList().get(2), is(2.2));
+    assertThat(shop.getPriceList().get(3), is(2.3));
+    assertThat(shop.getPriceList().get(4), is(6.2) );
   }
 }
